08cf779a5d6ad8965c61a5bb6d8b708a707a3a95
Mark Tonkinson <DHTMaster@gmail.com>
2014-10-14 14:39:33 -0600

    working appearance and trying to add a password for the games

--- views/creategame/creategameCtrl.js
+++ views/creategame/creategameCtrl.js
@@ -6,19 +6,44 @@ app.controller('creategameCtrl', function($scope, $location, playerService, crea
 		//$firebase(new Firebase(firebaseUrl + playerService.getUid() + '/gameCreator')); //this is what you did to add an answer list
 	
 		$scope.gameTitle;
+
+		$scope.password;
 		
 		$scope.games = $firebase(new Firebase(firebaseUrl + 'games')).$asObject();
 		
+		console.log($scope.games)
 		//this grabs the user info from the service
 		$scope.userName = playerService.getUserName();
 		$scope.userImage = playerService.getUserImage();
-		
-		$scope.addToGameArray = function(gameTitle) {
 
-			var gameRef = $firebase(new Firebase(firebaseUrl + 'games/' + gameTitle));
+		
+		
+		$scope.addToGameArray = function() {
 			
-			gameRef.$set(gameTitle);	
+			if($scope.password) {
+				var pword = $scope.password;				
+			} else{
+				pword = 'none'
+			}
+			var newGameObj = {
+				gameName: $scope.gameTitle,
+				gamePassword: pword
+			}
 			
+			debugger;
+			var gameRef = $firebase(new Firebase(firebaseUrl + 'games/' + newGameObj.gameName));
+			
+			gameRef.$set(newGameObj);
+
+			$scope.gameTitle='';
+			$scope.passwordInput = false;	
+			
+		}
+
+		$scope.passwordInput = false;
+
+		$scope.passwordInputShow = function() {
+			$scope.passwordInput = !$scope.passwordInput;
 		}
 		
 })
